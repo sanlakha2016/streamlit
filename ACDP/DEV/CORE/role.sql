9ceefcfc4991fa9199ebab622ab37325a7a08f66
sanjay.lakhanpal <sanjay.lakhanpal@mitre10.co.nz>
2024-06-08 13:30:57 +1200

    Initial Core Access to roles

---
+++ ACDP/DEV/CORE/role.sql
@@ -0,0 +1,6 @@
+/* Intial Role Creation */
+USE ROLE USERADMIN;
+Create Role if not exists ACDP_ENGINEER;
+Role if not exists ACDP_ADMIN;
+Role if not exists ACDP_ANALYST;
+Role if not exists ACDP_REPORT;
